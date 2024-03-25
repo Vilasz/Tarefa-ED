c5e987c4fbdd0ecef420dd4f5e3b39d620573c42
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2024-03-25 08:05:04 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,30 @@ using std::cout;
 using std::endl;
 using std::string;
 
+
+// Resolução Questão 1:
+int potencia(int iBase, int iExpoente){
+    int iResposta = 1;
+    for(int i=0; i<iExpoente; i++){
+        iResposta = iResposta * iBase;
+    }
+
+    return iResposta;
+}
+
 int main()
 {
-    return 0;
+    int iBase;
+    int iExpoente;
+    int iResposta;
 
+
+    std::cin >> iBase;
+    std::cin >> iExpoente;
+
+    iResposta = potencia(iBase, iExpoente);
+
+    std::cout << iResposta;
+
+    return 0;
 }
