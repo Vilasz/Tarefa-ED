716faa5e79f1c61b69dea89782fd6e82a19654ea
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2024-03-25 08:44:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,5 +29,67 @@ int main()
 
     cout << iResposta;
 
+    float fNumero;
+
+    cout << "Digite um número: ";
+    cin >> fNumero;
+
+    int num_dig = numdigitos(fNumero);
+
+    cout << "O número de dígitos em " << fNumero << " é: " << num_dig << endl;
+
+    int iPosFibonacci;
+
+    cout << "Digite a posição na sequência de Fibonacci: ";
+    cin >> iPosFibonacci;
+
+    int resultado = fibonacci(iPosFibonacci);
+
+    cout << "O número de Fibonacci na posição " << iPosFibonacci << " é: " << resultado << endl;
+
+    cout << "Digite o valor inicial: ";
+    cin >> iInicial;
+
+    cout << "Digite a razão: ";
+    cin >> iRazao;
+
+    cout << "Digite a posição: ";
+    cin >> iPosicao;
+
+    int iValorFinal = progressao_aritmetica(iInicial, iRazao, iPosicao);
+
+    cout << "O valor na posição " << iPosicao << " da progressão é: " << iValorFinal << endl;
+
+
     return 0;
+}
+
+int potencia(int& iBase, int& iExpoente){
+    int iResposta = 1;
+    for(int i = 0; i < iExpoente; i++){
+        iResposta *= iBase;
+    }
+    return iResposta;
+}
+
+int progressao_aritmetica(int& iInicial, int& iRazao, int& iPosicao) {
+    int iValorFinal;
+    iValorFinal = iInicial + iPosicao * iRazao;
+    return iValorFinal;
+}
+
+int fibonacci(int& iPosFibonacci) {
+    if (iPosFibonacci < 2) {
+        return 1;
+    }
+    return fibonacci(iPosFibonacci - 1) + fibonacci(iPosFibonacci - 2);
+}
+
+int numdigitos(float& fNumero) {
+    int iContador = 0;
+    while (fNumero >= 1) {
+        fNumero /= 10;
+        iContador++;
+    }
+    return iContador;
 }
